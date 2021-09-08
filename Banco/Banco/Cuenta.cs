22478d99c7375146aedb7f2362bb344b9eb54b12
LuxMG <lucecitagiuliani@gmail.com>
2021-09-08 17:52:08 -0300

    clases a medias

---
+++ Banco/Banco/Cuenta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public enum TipoDeExtraccion 
+    {
+        CajeroHumano = 1,
+        CajeroAutomatico = 2
+    };
+
+    public enum TipoDeCuenta
+    {
+        CuentaCorrientePesos,
+        CuentaCorrienteDolares,
+        CajaAhorroPesos
+    };
+
+    class Cuenta
+    {
+        protected double cantidadDinero;
+        protected TipoDeCuenta tipo;
+
+        public double CantidadDinero { get => cantidadDinero; set => cantidadDinero = value; }
+        public TipoDeCuenta Tipo { get => tipo; set => tipo = value; }
+
+        public void Insercion(int monto, TipoDeExtraccion tipoex)
+        {
+            
+        }
+        
+    }
+}
---
+++ Banco/Banco/CuentaPesos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class CuentaPesos : Cuenta
+    {
+
+        public new void Insercion(int monto, TipoDeExtraccion tipoex)
+        {
+            if (cantidadDinero - monto < -5000)
+                Console.WriteLine("Fondos insuficientes.");
+            else
+            {
+                if(tipoex == TipoDeExtraccion.CajeroHumano)
+                    cantidadDinero -= monto;
+                else
+                {
+                    if (monto > 20000)
+                        Console.WriteLine("No puede sacar mas de 20.000 pesos");
+                    else
+                        cantidadDinero -= monto;
+                }
+            }
+        }
+    }
+}
